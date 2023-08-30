2248b79be2c4abc8f368e1b4368c1b3f6058048a
rockofox <5049120+rockofox@users.noreply.github.com>
2023-08-30 13:49:55 +0200

    Create yabai.rb

---
+++ yabai.rb
@@ -0,0 +1,44 @@
+class Yabai < Formula
+  env :std
+  desc "Yabai with border above window"
+  homepage "https://github.com/rockofox/yabai"
+  head "https://github.com/rockofox/yabai.git"
+
+  depends_on :macos => :big_sur
+
+  def install
+    (var/"log/yabai").mkpath
+    man.mkpath
+
+    if build.head?
+      system "make", "-j1", "install"
+    end
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+
+    Logs will be found in
+      #{var}/log/yabai/yabai.[out|err].log
+
+    If you are using the scripting-addition; remember to update your sudoers file:
+      sudo visudo -f /private/etc/sudoers.d/yabai
+
+    Build the configuration row by running:
+      echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa"
+
+    README: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+    EOS
+  end
+
+  test do
+    assert_match "yabai-v#{version}", shell_output("#{bin}/yabai --version")
+  end
+end
